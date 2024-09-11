5bc8e60654544686475045c52dc7b2a55cb62115
dkdkdsa <bos94330@gmail.com>
2024-09-12 00:15:23 +0900

    초기 커밋.

---
+++ Assets/02_Scripts/Core/Support.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public static class Support
+{
+
+    public static int GetHash(this string value)
+    {
+
+        return value.GetHashCode();
+
+    }
+
+    public static T Clone<T>(this T source) where T : ICloneable
+    {
+
+        return (T)source.Clone();
+
+    }
+
+    public static T Cast<T>(this object source)
+    {
+
+        return (T)source;
+
+    }
+
+    public static int GetGameObjectId(this Component comp)
+    {
+
+        return comp.gameObject.GetInstanceID();
+
+    }
+
+    public static T GetRandom<T>(this List<T> target)
+    {
+
+        return target[Random.Range(0, target.Count)];
+
+    }
+
+}
