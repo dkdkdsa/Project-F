9b08b138757eb0d64c48872fec6a92e1215a09c9
dkdkdsa <bos94330@gmail.com>
2024-09-12 10:52:52 +0900

    플레이어 컨트롤러 코드 작성

--- Assets/02_Scripts/Game/Player/Control/PlayerController.cs
+++ Assets/02_Scripts/Game/Player/Control/PlayerController.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, ILocalInject
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private readonly int HASH_MOVE = "Move".GetHash();
+    private readonly int HASH_JUMP = "Jump".GetHash();
+
+    private IInputContainer _input;
+    private IMoveable _move;
+    private IJumpable _jump;
+
+    public void LocalInject(ComponentList list)
     {
-        
+
+        _input = list.Find<IInputContainer>();
+        _move = list.Find<IMoveable>();
+        _jump = list.Find<IJumpable>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
+    {
+
+        _input.RegisterEvent(HASH_JUMP, HandleJump);
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        _move.Move(_input.GetValue<Vector2>(HASH_MOVE), 3);
+
+    }
+
+    private void HandleJump(object obj)
+    {
+
+        _jump.Jump(3);
+
+    }
+
+    private void OnDestroy()
     {
         
+        if(_input != null)
+        {
+
+            _input.UnregisterEvent(HASH_JUMP, HandleJump);
+
+        }
+
     }
+
 }
