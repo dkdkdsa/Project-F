2c74fa66e22abe9a635d2fa61674b0f69fc95911
dkdkdsa <bos94330@gmail.com>
2024-09-12 10:16:22 +0900

    스펠링 수정 2

--- Assets/02_Scripts/Game/Physics/IPhysics.cs
+++ Assets/02_Scripts/Game/Physics/IPhysics.cs
@@ -5,6 +5,6 @@ public interface IPhysics
 
     public Vector3 GetVelocity();
     public void SetVelocity(Vector3 val);
-    public void AddForce(Vector3 fource, ForceMode2D mode = ForceMode2D.Impulse);
+    public void AddForce(Vector3 force, ForceMode2D mode = ForceMode2D.Impulse);
 
 }
