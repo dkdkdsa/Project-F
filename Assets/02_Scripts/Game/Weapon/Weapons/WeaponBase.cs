425fae8f23409da0fa9a442e293827584ec21c6b
dkdkdsa <bos94330@gmail.com>
2024-09-13 21:04:06 +0900

    무기 구조 작성중

--- Assets/02_Scripts/Game/Weapon/Weapons/WeaponBase.cs
+++ Assets/02_Scripts/Game/Weapon/Weapons/WeaponBase.cs
@@ -1,17 +1,48 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class WeaponBase : MonoBehaviour, IWeapon
+public abstract class WeaponBase : MonoBehaviour, IWeapon, ILocalInject
 {
+
+    #region Hash
+    protected readonly int HASH_COOL_TIME = "CoolTime".GetHash();
+    #endregion
+
+    private IStatContainer _stat;
+
+    protected bool _isCoolTime;
+
+    public float CoolTime => _stat[HASH_COOL_TIME].Value;
 
     public event Action<object> AttackEvent;
     public event Action<object> RotateEvent;
     public event Action<object> ReleaseEvent;
 
+    public virtual void LocalInject(ComponentList list)
+    {
+
+        _stat = list.Find<IStatContainer>();
+
+    }
+
     public void Attack()
     {
+
+        if (_isCoolTime)
+            return;
+
+        _isCoolTime = true;
+        TimerHelper.StartTimer(CoolTime).OnEndEvent += HandleCoolDownEnd;
+
+        DoAttack();
+
+    }
+
+    private void HandleCoolDownEnd(float t)
+    {
+
+        _isCoolTime = false;
+
     }
 
     public abstract void DoAttack();
@@ -23,5 +54,6 @@ public abstract class WeaponBase : MonoBehaviour, IWeapon
         Destroy(gameObject);
 
     }
+
 
 }
