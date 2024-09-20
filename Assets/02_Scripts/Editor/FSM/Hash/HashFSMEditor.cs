08fd721c68e5439c834401d0350776c1448edbe8
dkdkdsa <bos94330@gmail.com>
2024-09-20 17:07:59 +0900

    FSM에디터 코드 작성중

--- Assets/02_Scripts/Editor/FSM/Hash/HashFSMEditor.cs
+++ Assets/02_Scripts/Editor/FSM/Hash/HashFSMEditor.cs
@@ -1,18 +1,118 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class HashFSMEditor : MonoBehaviour
+public class HashFSMEditor : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    private const string UXML_PATH_ROOT = "Assets/07_ToolKit/FSM/Hash/";
+
+    [MenuItem("Tool/FSM")]
+    public static void OpenEditor()
     {
-        
+
+        CreateWindow<HashFSMEditor>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+
+        new MainWindowElement(rootVisualElement);
+
     }
+
+    private class MainWindowElement
+    {
+
+        /// <summary>
+        /// 현재 스테이트들이 보이는 필드
+        /// </summary>
+        private DropdownField _statesField;
+
+        /// <summary>
+        /// 스테이트 이름 생성할 때 이름 입력하는 필드
+        /// </summary>
+        private TextField _stateNameField;
+
+        /// <summary>
+        /// 스테이트 만들 때 클릭하는 버튼
+        /// </summary>
+        private Button _createStateButton;
+
+        /// <summary>
+        /// 현재 스테이트 라벨
+        /// </summary>
+        private Label _currentStateLabel;
+
+        /// <summary>
+        /// 바인딩할 state클래스를 선택하는 필드
+        /// </summary>
+        private DropdownField _bindStateField;
+
+        public event Action<string> OnCreateState;
+        public event Action<string> OnSelectStateChanged;
+
+        public MainWindowElement(VisualElement root)
+        {
+
+            var ins = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(UXML_PATH_ROOT + "HashFSMEditor.uxml");
+            VisualElement obj = ins.Instantiate();
+
+            root.Add(obj);
+
+            Binding(obj);
+
+        }
+
+        private void Binding(VisualElement obj)
+        {
+
+            _statesField = obj.Q<DropdownField>("StatesField");
+            _stateNameField = obj.Q<TextField>("StateNameField");
+            _createStateButton = obj.Q<VisualElement>("AddState").Q<Button>("AddButton");
+            _currentStateLabel = obj.Q<Label>("CurrentStateText");
+            _bindStateField = obj.Q<DropdownField>("BindStateField");
+
+            _createStateButton.clicked += HandleCreateState;
+            _statesField.RegisterValueChangedCallback(HandleSelectStateChange);
+
+        }
+
+        private void HandleSelectStateChange(ChangeEvent<string> evt)
+        {
+
+            _currentStateLabel.text = evt.newValue;
+            OnSelectStateChanged?.Invoke(evt.newValue);
+
+        }
+
+        private void HandleCreateState()
+        {
+
+            var name = _stateNameField.value;
+
+            _statesField.choices.Add(name);
+
+            OnCreateState?.Invoke(name);
+
+        }
+
+        /// <summary>
+        /// 바인딩할 클래스를 추가
+        /// </summary>
+        /// <param name="name">클래스 이름</param>
+        public void AddBindingClass(string name)
+        {
+
+            _bindStateField.choices.Add(name);
+
+        }
+
+    }
+
 }
